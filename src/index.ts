a20bdc74044e522508bc096b67206cf12098a9d4
benebene84 <benedikt.sperl@gmail.com>
2025-03-27 14:30:14 +0100

    chore: add get variants to export file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { Button } from "./components/button/button";
-export { Button };
+export { Button } from "./components/button/button";
+export { getVariants } from "./utilities/get-variants/get-variants";
