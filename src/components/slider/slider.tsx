fe0a56ee6d1fa9f53db547e4b711c45a7a8d2c6d
benebene84 <benedikt.sperl@gmail.com>
2025-04-27 19:02:50 +0200

    fix: rename value to thumb value

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -36,14 +36,14 @@ const Slider = forwardRef<ComponentRef<typeof RadixSlider.Root>, SliderProps>(
 					<RadixSlider.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-gray-700/20 data-[disabled]:opacity-50">
 						<RadixSlider.Range className="absolute h-full bg-gray-700" />
 					</RadixSlider.Track>
-					{value.map((value, index) => (
+					{value.map((thumbValue, index) => (
 						// biome-ignore lint/suspicious/noArrayIndexKey: <only index is available>
 						<Tooltip key={index} delayDuration={0}>
 							<TooltipTrigger asChild>
 								<RadixSlider.Thumb className="block h-4 w-4 rounded-full border border-gray-700 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-700 data-[disabled]:pointer-events-none" />
 							</TooltipTrigger>
 							<TooltipContent>
-								<p>{value}</p>
+								<p>{thumbValue}</p>
 							</TooltipContent>
 						</Tooltip>
 					))}
