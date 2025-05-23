36a2706a7516d7b0f50b19764ec906cdd5ac3980
benebene84 <62141265+benebene84@users.noreply.github.com>
2025-05-23 11:34:21 +0200

    chore: add proper error handling to build script (#38)

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,32 +4,44 @@ import { execSync } from "node:child_process";
 import { existsSync, rmSync } from "node:fs";
 
 const startTime = performance.now();
+const color = await import("./color.ts").then((m) => m.color);
 
-console.log("Cleaning output directory...");
-if (existsSync("dist")) {
-	rmSync("dist", { recursive: true, force: true });
+try {
+	console.log("Cleaning output directory...");
+	if (existsSync("dist")) {
+		rmSync("dist", { recursive: true, force: true });
+	}
+	console.log("Done!");
+
+	const startTimeCompile = performance.now();
+	console.log("\nCompiling TypeScript...");
+	execSync("pnpm tsc -p ./tsconfig.build.json", { stdio: "inherit" });
+	execSync("pnpm tsc-alias -p ./tsconfig.build.json", { stdio: "inherit" });
+
+	const endTimeCompile = performance.now();
+	const durationCompile = (endTimeCompile - startTimeCompile) / 1000;
+	console.log(
+		`Done! ${color(`(${durationCompile.toFixed(2)} seconds)`, "blue")}`,
+	);
+
+	const startTimeCopy = performance.now();
+	console.log("\nCopying assets...");
+	const copyAssets = await import("./copy-build-assets.ts");
+	await copyAssets.default;
+	const endTimeCopy = performance.now();
+	const durationCopy = (endTimeCopy - startTimeCopy) / 1000;
+	console.log(`Done! ${color(`(${durationCopy.toFixed(2)} seconds)`, "blue")}`);
+
+	const endTime = performance.now();
+	const duration = (endTime - startTime) / 1000;
+
+	console.log(
+		`\n${color(
+			`Build completed successfully in ${duration.toFixed(2)} seconds!`,
+			"green",
+		)} 🎉`,
+	);
+} catch (error) {
+	console.error(color(`Error: ${error}`, "red"));
+	process.exit(1);
 }
-console.log("Done!");
-
-const startTimeCompile = performance.now();
-console.log("\nCompiling TypeScript...");
-execSync("pnpm tsc -p ./tsconfig.build.json", { stdio: "inherit" });
-execSync("pnpm tsc-alias -p ./tsconfig.build.json", { stdio: "inherit" });
-
-const endTimeCompile = performance.now();
-const durationCompile = (endTimeCompile - startTimeCompile) / 1000; // Convert to seconds
-console.log(`Done! \x1b[34m(${durationCompile.toFixed(2)} seconds)\x1b[0m`);
-
-const startTimeCopy = performance.now();
-console.log("\nCopying assets...");
-await import("./copy-build-assets.ts");
-const endTimeCopy = performance.now();
-const durationCopy = (endTimeCopy - startTimeCopy) / 1000; // Convert to seconds
-console.log(`Done! \x1b[34m(${durationCopy.toFixed(2)} seconds)\x1b[0m`);
-
-const endTime = performance.now();
-const duration = (endTime - startTime) / 1000; // Convert to seconds
-
-console.log(
-	`\n\x1b[32mBuild completed successfully in ${duration.toFixed(2)} seconds!\x1b[0m 🎉`,
-);
--- scripts/copy-build-assets.ts
+++ scripts/copy-build-assets.ts
@@ -1,6 +1,6 @@
 import { cp, mkdir } from "node:fs/promises";
 import { dirname } from "node:path";
-
+import { color } from "./color.ts";
 // Source, destination, recursive
 const dirs: [string, string, boolean][] = [["src/styles", "dist/styles", true]];
 
@@ -13,14 +13,14 @@ async function copyAssets(
 	await cp(source, destination, { recursive });
 }
 
-Promise.all(
+export default Promise.all(
 	dirs.map(async ([source, destination, recursive]) => {
 		console.log(`Copying "${source}" to "${destination}"`);
 		try {
 			await copyAssets(source, destination, recursive);
 		} catch (error) {
 			console.error(
-				`\x1b[31mError copying "${source}" to "${destination}": ${error}\x1b[0m`,
+				color(`Error copying "${source}" to "${destination}": ${error}`, "red"),
 			);
 			process.exit(1);
 		}
