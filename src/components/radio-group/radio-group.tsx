f7be20725739e4be5303c76ee570b2db90ef0504
benebene84 <benedikt.sperl@gmail.com>
2025-12-25 20:42:19 +0100

    feat: radio group - switch to base ui

--- src/components/radio-group/radio-group.tsx
+++ src/components/radio-group/radio-group.tsx
@@ -1,11 +1,8 @@
+import { Radio } from "@base-ui/react/radio";
+import { RadioGroup as BaseRadioGroup } from "@base-ui/react/radio-group";
 import { useFormContext } from "@components/form-field/form-field-context";
 import { cn } from "@utilities/cn/cn";
-import { RadioGroup as RadixRadioGroup } from "radix-ui";
-import {
-	type ComponentPropsWithoutRef,
-	type ComponentRef,
-	forwardRef,
-} from "react";
+import { type ComponentPropsWithoutRef, forwardRef } from "react";
 
 /**
  * A RadioGroup component that provides a set of radio buttons where only one can be selected at a time.
@@ -31,14 +28,11 @@ import {
  * @param {boolean} [disabled] - When true, prevents the user from interacting with the radio group
  * @param {boolean} [required] - When true, indicates that the user must select a value before the owning form can be submitted
  * @param {string} [name] - The name of the group. Submitted with its owning form as part of a name/value pair
- * @param {string} [dir] - The reading direction of the radio group
- * @param {string} [orientation] - The orientation of the radio group
- * @param {boolean} [loop] - When true, keyboard navigation will loop from last item to first, and vice versa
  * @returns {React.ForwardRefExoticComponent} A React component that renders a radio group
  */
 const RadioGroup = forwardRef<
-	ComponentRef<typeof RadixRadioGroup.Root>,
-	ComponentPropsWithoutRef<typeof RadixRadioGroup.Root>
+	HTMLDivElement,
+	ComponentPropsWithoutRef<typeof BaseRadioGroup>
 >(
 	(
 		{
@@ -54,7 +48,7 @@ const RadioGroup = forwardRef<
 			required: initialRequired,
 		});
 		return (
-			<RadixRadioGroup.Root
+			<BaseRadioGroup
 				ref={ref}
 				className={cn("flex flex-col gap-2", className)}
 				disabled={disabled}
@@ -66,19 +60,19 @@ const RadioGroup = forwardRef<
 );
 
 const RadioGroupItem = forwardRef<
-	ComponentRef<typeof RadixRadioGroup.Item>,
-	ComponentPropsWithoutRef<typeof RadixRadioGroup.Item>
+	HTMLButtonElement,
+	ComponentPropsWithoutRef<typeof Radio.Root>
 >(({ className, ...props }, ref) => (
-	<RadixRadioGroup.Item
+	<Radio.Root
 		ref={ref}
 		className={cn(
-			"flex h-4 w-4 items-center justify-center rounded-full border border-gray-700 bg-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50",
+			"flex h-4 w-4 items-center justify-center rounded-full border border-gray-700 bg-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 data-disabled:cursor-not-allowed data-disabled:opacity-50",
 			className,
 		)}
 		{...props}
 	>
-		<RadixRadioGroup.Indicator className="relative h-2 w-2 rounded-full bg-gray-500" />
-	</RadixRadioGroup.Item>
+		<Radio.Indicator className="relative h-2 w-2 rounded-full bg-gray-500" />
+	</Radio.Root>
 ));
 
 export { RadioGroup, RadioGroupItem };
