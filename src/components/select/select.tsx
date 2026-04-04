dc7841a5068b59226908852a5703dac25c5e6ee8
Benedikt Sperl <62141265+benebene84@users.noreply.github.com>
2026-04-04 15:38:46 +0200

    feat: add light and dark theming (#67)

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -61,7 +61,7 @@ function SelectTrigger({
 		<SelectPrimitive.Trigger
 			ref={ref}
 			className={cn(
-				"flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-placeholder:text-text-muted [&>span]:line-clamp-1",
+				"flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-border-input bg-surface px-3 py-2 text-sm text-text shadow-sm ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-placeholder:text-text-muted [&>span]:line-clamp-1",
 				className,
 			)}
 			{...props}
